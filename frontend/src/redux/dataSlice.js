e92f505426f8954f21eebc681161e931337d2e07
Krutik Bundela <krutikbundela2001@gmail.com>
2024-10-20 01:11:26 +0530

    added deployed backend url to frontend

--- frontend/src/redux/dataSlice.js
+++ frontend/src/redux/dataSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://email-visualization.onrender.com/api/v1",
 });
 
 export const fetchData = createAsyncThunk(
